9c0bebb483c9c2db9bf83f0eb8b884e30730c6f6
Bill Cathey <billcathey@hotmail.com>
2021-08-30 17:56:58 -0400

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlesList from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
