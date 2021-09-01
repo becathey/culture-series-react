6aeebfb5584d380b5781700932c0f1f818b37fe0
Bill Cathey <billcathey@hotmail.com>
2021-09-01 14:28:58 -0400

    Add ArticlesList with links to articles

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from "./article-content";
+
 const ArticlesList = () => (
   <div>
     <h1>Articles</h1>
+    {articleContent.map((article, i) => (
+      <Link
+        className="article-list-item"
+        key={i}
+        to={`/articles/${article.name}`}
+      >
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0, 150)} ...</p>
+      </Link>
+    ))}
   </div>
 );
 export default ArticlesList;
