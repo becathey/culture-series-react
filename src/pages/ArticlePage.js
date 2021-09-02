dfb4d5dba61f67d1e7c174bb0fc271ab2bce0fbe
Bill Cathey <billcathey@hotmail.com>
2021-09-01 20:22:10 -0400

    Add Other Articles section on ArticlePage component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ArticlesList from "../components/ArticlesList";
 import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
   if (!article) return <h1>Article does not exist!</h1>;
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
   return (
     <div>
       <h1>{article.title}</h1>
@@ -12,6 +16,8 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <h3>Other Articles</h3>
+      <ArticlesList articles={otherArticles} />
     </div>
   );
 };
