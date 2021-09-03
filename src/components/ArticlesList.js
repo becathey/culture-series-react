12e0c6e9a2a420176cb803ae6f756ac48e7360aa
Bill Cathey <billcathey@hotmail.com>
2021-09-02 22:07:56 -0400

    Add images to ArticlesList, fine-tune styles

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -5,12 +5,17 @@ const ArticlesList = ({ articles }) => (
   <div>
     {articles.map((article, i) => (
       <Link
-        className="article-list-item"
+        className="article-list-link"
         key={i}
         to={`/articles/${article.name}`}
       >
-        <h3>{article.title}</h3>
-        <p>{article.content[0].substring(0, 150)} ...</p>
+        <div className="article-list-item">
+          <img src={article.image} alt="" />
+          <div>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)} ...</p>
+          </div>
+        </div>
       </Link>
     ))}
   </div>
