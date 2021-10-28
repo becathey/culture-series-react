aa90ceed8ce0b35c614f497b1a475d3d26470e36
Bill Cathey <billcathey@hotmail.com>
2021-10-28 00:45:12 -0400

    Add image to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,39 +2,10 @@ import React from "react";
 const HomePage = () => (
   <div>
     <h1>The Culture Series</h1>
-    <p>
-      The Culture series consists of 9 novels and 1 book of short fiction
-      featuring a fictional interstellar post-scarcity civilization created by
-      the Scottish writer Iain M. Banks.
-    </p>
-    <p>
-      The Culture is composed primarily of sentient humanoid aliens,
-      artificially intelligent sentient machines, and a small number of other
-      sentient "alien" life forms. Machine intelligences range from
-      human-equivalent drones to hyper-intelligent Minds. Artificial
-      intelligences with capabilities measured as a fraction of human
-      intelligence also perform a variety of tasks.
-    </p>
-    <p>
-      Many of the series' protagonists are humanoids who choose to work for the
-      Culture's elite diplomatic or espionage organizations, and interact with
-      other civilizations whose citizens hold wildly different ideologies,
-      morals, and technologies.
-    </p>
-    <p>
-      Most of the Culture's citizens do not live on planets but in artificial
-      habitats such as orbitals and ships, the largest of which are home to
-      billions of individuals. The Culture's citizens have been genetically
-      enhanced to live for centuries and have modified mental control over their
-      physiology, including the ability to introduce a variety of psychoactive
-      drugs into their systems, change biological sex, or switch off pain at
-      will.
-    </p>
-    <p>
-      The Culture holds peace and individual freedom as core values, and a
-      central theme of the series is ethical struggles it faces when interacting
-      with other societies.
-    </p>
+    <img
+      src="http://billcathey.com/projects/assets/culture-images/culture-novels_700.jpg"
+      alt="The Culture Novels"
+    />
   </div>
 );
 export default HomePage;
