eddbc8440dddd86c4e5ccf345e830d7766a91594
Bill Cathey <billcathey@hotmail.com>
2021-10-28 00:48:26 -0400

    Change nav link from Articles to Books

--- src/NavBar.js
+++ src/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => (
         <Link to="/about">About</Link>
       </li>
       <li>
-        <Link to="/articles-list">Articles</Link>
+        <Link to="/articles-list">Books</Link>
       </li>
     </ul>
   </nav>
