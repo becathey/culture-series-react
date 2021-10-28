9908257b26071949be9c6fdbfcfa353313b99d01
Bill Cathey <billcathey@hotmail.com>
2021-10-28 00:37:30 -0400

    Add text to About page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,16 +3,37 @@ const AboutPage = () => (
   <div>
     <h1>About</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Augue eget arcu dictum
-      varius duis at consectetur lorem. Massa enim nec dui nunc. Cursus mattis
-      molestie a iaculis at. Pulvinar pellentesque habitant morbi tristique. At
-      tempor commodo ullamcorper a lacus vestibulum sed. Quisque non tellus orci
-      ac auctor augue mauris. Interdum velit euismod in pellentesque massa
-      placerat duis ultricies. Nulla at volutpat diam ut venenatis tellus in.
-      Pellentesque elit eget gravida cum. Ut tortor pretium viverra suspendisse
-      potenti nullam ac. Integer quis auctor elit sed vulputate. Lectus quam id
-      leo in vitae. Turpis egestas sed tempus urna.
+      The Culture series consists of 9 novels and 1 book of short fiction
+      featuring a fictional interstellar post-scarcity civilization created by
+      the Scottish writer Iain M. Banks.
+    </p>
+    <p>
+      The Culture is composed primarily of sentient humanoid aliens,
+      artificially intelligent sentient machines, and a small number of other
+      sentient "alien" life forms. Machine intelligences range from
+      human-equivalent drones to hyper-intelligent Minds. Artificial
+      intelligences with capabilities measured as a fraction of human
+      intelligence also perform a variety of tasks.
+    </p>
+    <p>
+      Many of the series' protagonists are humanoids who choose to work for the
+      Culture's elite diplomatic or espionage organizations, and interact with
+      other civilizations whose citizens hold wildly different ideologies,
+      morals, and technologies.
+    </p>
+    <p>
+      Most of the Culture's citizens do not live on planets but in artificial
+      habitats such as orbitals and ships, the largest of which are home to
+      billions of individuals. The Culture's citizens have been genetically
+      enhanced to live for centuries and have modified mental control over their
+      physiology, including the ability to introduce a variety of psychoactive
+      drugs into their systems, change biological sex, or switch off pain at
+      will.
+    </p>
+    <p>
+      The Culture holds peace and individual freedom as core values, and a
+      central theme of the series is ethical struggles it faces when interacting
+      with other societies.
     </p>
   </div>
 );
