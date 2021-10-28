6d163fcedb8125efb3e8e22f3a3b387d185a2f2b
Bill Cathey <billcathey@hotmail.com>
2021-10-28 01:20:35 -0400

    Add data file to repo

---
+++ src/pages/article-content.js
@@ -0,0 +1,124 @@
+const articles = [
+  {
+    name: "consider-phlebas",
+    title: "Consider Phlebas",
+    published: "1987",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/consider-phlebas.jpg",
+    content: [
+      `The first book in Iain M. Banks's seminal science fiction series, The Culture. Consider Phlebas introduces readers to the utopian conglomeration of human and alien races that explores the nature of war, morality, and the limitless bounds of mankind's imagination.`,
+      `The war raged across the galaxy. Billions had died, billions more were doomed. Moons, planets, the very stars themselves, faced destruction, cold-blooded, brutal, and worse, random. The Idirans fought for their Faith; the Culture for its moral right to exist. Principles were at stake. There could be no surrender.`,
+      `Within the cosmic conflict, an individual crusade. Deep within a fabled labyrinth on a barren world, a Planet of the Dead proscribed to mortals, lay a fugitive Mind. Both the Culture and the Idirans sought it. It was the fate of Horza, the Changer, and his motley crew of unpredictable mercenaries, human and machine, actually to find it, and with it their own destruction.`,
+    ],
+  },
+  {
+    name: "player-of-games",
+    title: "The Player of Games",
+    published: "1988",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/player-of-games.jpg",
+    content: [
+      `The Culture -- a human/machine symbiotic society -- has thrown up many great Game Players, and one of the greatest is Gurgeh. Jernau Morat Gurgeh. The Player of Games. Master of every board, computer and strategy. Bored with success, Gurgeh travels to the Empire of Azad, cruel and incredibly wealthy, to try their fabulous game. . .a game so complex, so like life itself, that the winner becomes emperor. Mocked, blackmailed, almost murdered, Gurgeh accepts the game, and with it the challenge of his life -- and very possibly his death.`,
+    ],
+  },
+  {
+    name: "use-of-weapons",
+    title: "Use of Weapons",
+    published: "1990",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/use-of-weapons.jpg",
+    content: [
+      `The man known as Cheradenine Zakalwe was one of Special Circumstances' foremost agents, changing the destiny of planets to suit the Culture through intrigue, dirty tricks and military action.`,
+      `The woman known as Diziet Sma had plucked him from obscurity and pushed him towards his present eminence, but despite all their dealings she did not know him as well as she thought.`,
+      `The drone known as Skaffen-Amtiskaw knew both of these people. It had once saved the woman's life by massacring her attackers in a particularly bloody manner. It believed the man to be a lost cause. But not even its machine could see the horrors in his past.`,
+      `Ferociously intelligent, both witty and horrific, USE OF WEAPONS is a masterpiece of science fiction.`,
+    ],
+  },
+  {
+    name: "state-of-the-art",
+    title: "The State of the Art",
+    published: "1991",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/state-of-the-art.jpg",
+    content: [
+      `The State of the Art is a short story collection which includes some stories originally published under Banks' non-SF byline "Iain Banks", as well as the title novella and others set in Banks's Culture fictional universe.`,
+      `"The State of the Art", the title novella, chronicles a Culture mission to Earth in the late 1970s, and also serves as a prequel of sorts to Use of Weapons by featuring two of that novel's characters.`,
+      `In "A Gift from the Culture", a man in self-imposed exile from the Culture is pressured into committing an act of terrorism with the help of Culture technology.`,
+      `In "Descendant", a Culture soldier and his sapient spacesuit struggle to survive after being shot down and crash-landing on a barren planet.`,
+      `Non-Culture stories include: "Road of Skulls", "Odd Attachment", "Cleaning Up", "Piece", and "Scratch".`,
+    ],
+  },
+  {
+    name: "excession",
+    title: "Excession",
+    published: "1996",
+    image: "http://billcathey.com/projects/assets/culture-images/excession.jpg",
+    content: [
+      `Diplomat Byr Genar-Hofoen is swept into a vast conspiracy that could lead the universe to the brink of annihilation when he is selected to investigate the disappearance of an ancient star.
+    DescriptionDiplomat Byr Genar-Hofoen has been selected by the Culture to undertake a delicate and dangerous mission. The Department of Special Circumstances--the Culture's espionage and dirty tricks section--has sent him off to investigate a 2,500-year-old mystery: the sudden disappearance of a star fifty times older than the universe itself. But in seeking the secret of the lost sun, Byr risks losing himself.`,
+      `There is only one way to break the silence of millennia: steal the soul of the long-dead starship captain who first encountered the star, and convince her to be reborn. And in accepting this mission, Byr will be swept into a vast conspiracy that could lead the universe into an age of peace...or to the brink of annihilation.`,
+    ],
+  },
+  {
+    name: "inversions",
+    title: "Inversions",
+    published: "1998",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/inversions.jpg",
+    content: [
+      `Iain M. Banks, the international bestselling author of The Player of Games and Consider Phlebas, is a true original, a literary visionary whose brilliant speculative fiction has transported us into worlds of unbounded imagination. Now, in his acclaimed new novel, Banks presents an engrossing portrait of an alien world, and of two very different people bound by a startling and mysterious secret.`,
+      `On a backward world with six moons, an alert spy reports on the doings of one Dr. Vosill, who has mysteriously become the personal physician to the king despite being a foreigner and, even more unthinkably, a woman. Vosill has more enemies than she first realizes. But then she also has more remedies in hand than those who wish her ill can ever guess.`,
+      `Elsewhere, in another palace across the mountains, a man named DeWar serves as chief bodyguard to the Protector General of Tassasen, a profession he describes as the business of "assassinating assassins." DeWar, too, has his enemies, but his foes strike more swiftly, and his means of combating them are more direct.`,
+      `No one trusts the doctor, and the bodyguard trusts no one, but is there a hidden commonality linking their disparate histories? Spiraling around a central core of mystery, deceit, love, and betrayal. Inversions is a dazzling work of science fiction from a versatile and imaginative author writing at the height of his remarkable powers.`,
+    ],
+  },
+  {
+    name: "look-to-windward",
+    title: "Look to Windward",
+    published: "2000",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/look-to-windward.jpg",
+    content: [
+      `The Twin Novae battle had been one of the last of the Idiran war, and one of the most horrific: desperate to avert their inevitable defeat, the Idirans had induced not one but two suns to explode, snuffing out worlds and biospheres teeming with sentient life. They were attacks of incredible proportion--gigadeathcrimes. But the war ended, and life went on.`,
+      `Now, eight hundred years later, light from the first explosion is about to reach the Masaq' Orbital, home to the Culture's most adventurous and decadent souls. There it will fall upon Masaq's 50 billion inhabitants, gathered to commemorate the deaths of the innocent and to reflect, if only for a moment, on what some call the Culture's own complicity in the terrible event.`,
+      `Also journeying to Masaq' is Major Quilan, an emissary from the war-ravaged world of Chel. In the aftermath of the conflict that split his world apart, most believe he has come to Masaq' to bring home Chel's most brilliant star and self-exiled dissident, the honored Composer Ziller.`,
+      `Ziller claims he will do anything to avoid a meeting with Major Quilan, who he suspects has come to murder him. But the Major's true assignment will have far greater consequences than the death of a mere political dissident, as part of a conspiracy more ambitious than even he can know--a mission his superiors have buried so deeply in his mind that even he cannot remember it.`,
+    ],
+  },
+  {
+    name: "matter",
+    title: "Matter",
+    published: "2008",
+    image: "http://billcathey.com/projects/assets/culture-images/matter.jpg",
+    content: [
+      `In a world renowned even within a galaxy full of wonders, a crime within a war. For one brother it means a desperate flight, and a search for the one -- maybe two -- people who could clear his name. For his brother it means a life lived under constant threat of treachery and murder. And for their sister, even without knowing the full truth, it means returning to a place she'd thought abandoned forever.`,
+      `Only the sister is not what she once was; Djan Seriy Anaplian has changed almost beyond recognition to become an agent of the Culture's Special Circumstances section, charged with high-level interference in civilizations throughout the greater galaxy.`,
+      `Concealing her new identity -- and her particular set of abilities -- might be a dangerous strategy, however. In the world to which Anaplian returns, nothing is quite as it seems; and determining the appropriate level of interference in someone else's war is never a simple matter.`,
+    ],
+  },
+  {
+    name: "surface-detail",
+    title: "Surface Detail",
+    published: "2010",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/surface-detail.jpg",
+    content: [
+      `Lededje Y'breq is one of the Intagliated, her marked body bearing witness to a family shame, her life belonging to a man whose lust for power is without limit. Prepared to risk everything for her freedom, her release, when it comes, is at a price, and to put things right she will need the help of the Culture.`,
+      `Benevolent, enlightened and almost infinitely resourceful though it may be, the Culture can only do so much for any individual. With the assistance of one of its most powerful -- and arguably deranged -- warships, Lededje finds herself heading into a combat zone not even sure which side the Culture is really on. A war -- brutal, far-reaching -- is already raging within the digital realms that store the souls of the dead, and it's about to erupt into reality.`,
+      `It started in the realm of the Real and that is where it will end. It will touch countless lives and affect entire civilizations, but at the center of it all is a young woman whose need for revenge masks another motive altogether.`,
+    ],
+  },
+  {
+    name: "hydrogen-sonata",
+    title: "The Hydrogen Sonata",
+    published: "2012",
+    image:
+      "http://billcathey.com/projects/assets/culture-images/hydrogen-sonata.jpg",
+    content: [
+      `An ancient people, organized on military principles and yet almost perversely peaceful, the Gzilt helped set up the Culture ten thousand years earlier and were very nearly one of its founding societies, deciding not to join only at the last moment. Now they've made the collective decision to follow the well-trodden path of millions of other civilizations; they are going to Sublime, elevating themselves to a new and almost infinitely more rich and complex existence.`,
+      `Amid preparations though, the Regimental High Command is destroyed. Lieutenant Commander (reserve) Vyr Cossont appears to have been involved, and she is now wanted -- dead, not alive. Aided only by an ancient, reconditioned android and a suspicious Culture avatar, Cossont must complete her last mission given to her by the High Command. She must find the oldest person in the Culture, a man over nine thousand years old, who might have some idea what really happened all that time ago.`,
+      `It seems that the final days of the Gzilt civilization are likely to prove its most perilous.`,
+    ],
+  },
+];
+export default articles;
